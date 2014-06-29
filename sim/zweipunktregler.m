ef97dd434def0180abaebacf52c4a19f06ff11e2
fltdpl <tom.dobs@fltdpl.de>
2014-06-30 01:06:48 +0200

    Berechnung und Doku angepasst

--- sim/zweipunktregler.m
+++ sim/zweipunktregler.m
@@ -20,11 +20,12 @@ N     = 4000;                 % Anzahl der Daten (= sek)
 t_min = [N/60/N:N/60/N:N/60]; % Zeit in Minuten
 state = 0;                    % 0 = Abkuehlen, 1 = Aufheizen
 hr    = 0.05;                  % Aufheizrate
-cr    = -0.0005;              % Abkuehlrate (geschaetzt mit Iso)
+%cr    = 0.0005;              % Abkuehlrate (geschaetzt mit Iso)
+cr    = 9.2E-4;               % Abkuehlrate: 40 Liter-Boiler
 Tbs   = zeros(1,N);           % Speichervektor fuer Boilertemp
 stas  = zeros(1,N);           % Speichervektor fuer den Zustand
 
-KH    = 0.05;                 % Kopplungsfaktor der Hysterese
+KH    = 0.1;                 % Kopplungsfaktor der Hysterese
 
 for i = 1:N
   if Tb >= (Tset + Tset*KH)   % Kriterium Kuehlen
@@ -36,8 +37,7 @@ for i = 1:N
 
   switch state                % Temperaturreaktion simulieren
   case 0                      % Kuehlen
-    %Tb = Tb + cr;
-    Tb = Tb + cr*(Tb - Tu);   % Netonsches Abkuehlungsgesetz
+    Tb = Tb - cr*(Tb - Tu);   % Netonsches Abkuehlungsgesetz
   case 1                      % Heizen
     Tb = Tb + hr;
   end
@@ -49,8 +49,12 @@ end
 
 % Plotten
 figure()
-plot(t_min, Tbs, t_min, stats)
+plot(t_min, Tbs,'linewidth', 3, 'Color', [.3 .3 .3], ...
+     t_min, stats, 'linewidth', 3, 'Color', [0 0 1])
 xlim([0 50])
 xlabel('Zeit in Minuten')
 ylabel('Temperatur in C')
+legend('Boilertemperatur', 'Zustand des Relais', 'location', 'East');
 grid on
+set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3])
+print('Zweipunktregler','-dtex','-r130');
