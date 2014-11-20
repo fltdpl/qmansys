90f9e776bec66ac72476a2f7aaf5217a06b7144a
fltdpl <tom.dobs@fltdpl.de>
2014-11-20 01:05:59 +0100

    SMS-Abfrage mit Progresskreisel. Anzeigen der Informationen aus eingehender SMS

--- Android/src/de/fltdpl/qmansis/SmsReceiver.java
+++ Android/src/de/fltdpl/qmansis/SmsReceiver.java
@@ -3,7 +3,9 @@ package de.fltdpl.qmansis;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -30,20 +32,29 @@ public class SmsReceiver extends BroadcastReceiver {
                 for (int i = 0; i < pdusObj.length; i++) {
                      
                     SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
-                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
-                     
-                    String senderNum = phoneNumber;
-                    String message = currentMessage.getDisplayMessageBody();
+                    String phoneNumber = currentMessage.getOriginatingAddress();
+                    String message = currentMessage.getMessageBody().toString();
  
-                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
-                     
- 
-                   // Show Alert
-                    int duration = Toast.LENGTH_LONG;
-                    Toast toast = Toast.makeText(context,
-                                 "senderNum: "+ senderNum + ", message: " + message, duration);
-                    toast.show();
-                     
+                    Log.i("SmsReceiver", "senderNum: "+ phoneNumber + "; message: " + message);
+                    
+                    // Nummer pruefen
+                    SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+                	String Telnummer    = getPrefs.getString("editnumber_preference", "0");
+                    
+                    if (Telnummer.equals(phoneNumber)) {
+                    	// Show Alert
+                        //int duration = Toast.LENGTH_LONG;
+                        //Toast toast = Toast.makeText(context,
+                        //             "senderNum: "+ senderNum + ", message: " + message, duration);
+                        //toast.show();
+                    	
+                        // A custom Intent that will used as another Broadcast
+                        Intent in = new Intent("SmsMessage.intent.STATUS").putExtra(
+                                "get_msg", phoneNumber + ":" + message);
+
+                        // send another broadcast
+                        context.sendBroadcast(in);
+                    }
                 } // end for loop
               } // bundle is null
  
--- Android/src/de/fltdpl/qmansis/Status.java
+++ Android/src/de/fltdpl/qmansis/Status.java
@@ -1,20 +1,32 @@
 package de.fltdpl.qmansis;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.telephony.SmsManager;
 
 public class Status extends ActionBarActivity {
 	
+	private BroadcastReceiver mIntentReceiver;	
+	private ProgressBar progressBar;
 	Button status;
+	TextView displaystatus;
+	TextView displaytempmotor;
+	TextView displaytempboiler;
+	
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,14 +61,70 @@ public class Status extends ActionBarActivity {
         setContentView(R.layout.activity_status);
         
         status = (Button) findViewById(R.id.button_status);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        progressBar.setVisibility(4);
         status.setOnClickListener(new View.OnClickListener() {
         	
         	public void onClick(View v) {
         		sendSMS_STATUS();
+        		progressBar.setVisibility(0);
+        		
+        		
+        		
         	}
         		
         });
     }
+    
+    @Override
+    protected void onResume() {
+           super.onResume();
+           
+           IntentFilter intentFilter = new IntentFilter("SmsMessage.intent.STATUS");
+           mIntentReceiver = new BroadcastReceiver() {
+        	   
+        	   @Override
+               public void onReceive(Context context, Intent intent) {
+        		   String msg = intent.getStringExtra("get_msg");
+        		   
+        		   // Process the sms format and extract body and phoneNumber
+        		   msg = msg.replace("\n", "/");
+        		   String body = msg.substring(msg.lastIndexOf(":") + 1, msg.length());
+        		   String pNumber = msg.substring(0, msg.lastIndexOf(":"));
+        		   // Debugging
+        		   Log.e("onResume", "" + msg + body + pNumber);
+        		   
+	               String bodytemp     = body.substring(0, body.lastIndexOf("/"));
+	               String instatus     = bodytemp.substring(0, bodytemp.lastIndexOf("/"));
+	               String intempmotor  = bodytemp.substring(bodytemp.lastIndexOf("/") + 1, bodytemp.length());
+	               String intempboiler = body.substring(body.lastIndexOf("/") +1, body.length());
+	               
+	               displaystatus     = (TextView) findViewById(R.id.text_status_pumpe_2);
+	               displaytempmotor  = (TextView) findViewById(R.id.text_motortemperatur_2);
+	               displaytempboiler = (TextView) findViewById(R.id.text_boilertemperatur_2);
+	               if (instatus.equals("ON")) {
+	            	   displaystatus.setText("An");
+	               }else{
+	            	   displaystatus.setText("Aus");
+	               };
+	               displaytempmotor.setText(intempmotor + "°C");
+	               displaytempboiler.setText(intempboiler + "°C");
+	               
+	               progressBar.setVisibility(8);
+	               
+        	   }
+        	   
+           };
+           this.registerReceiver(mIntentReceiver, intentFilter);
+    }
+    
+    @Override
+    protected void onPause() {
+
+           super.onPause();
+           this.unregisterReceiver(this.mIntentReceiver);
+    }
+    
     
     public void sendSMS_STATUS() {
     	
@@ -67,6 +135,10 @@ public class Status extends ActionBarActivity {
     	
     	SmsManager smsManager = SmsManager.getDefault();
     	smsManager.sendTextMessage(Telnummer, null, Telnachricht, null, null);
+    	
+    	// Debugging
+    	Log.e("sendSMS_STATUS", Telnummer + " / " + Telnachricht);
+    	
     	
     }
     
