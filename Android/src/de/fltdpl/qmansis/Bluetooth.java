c7bc59a5056f0dd8e007831f91ca740d27a6c2a7
fltdpl <tom.dobs@fltdpl.de>
2015-01-02 23:34:19 +0100

    Bluetooth: Abfrage, ob Bluetooth eingeschaltet ist und Bluetooth einschalten

--- Android/src/de/fltdpl/qmansis/Bluetooth.java
+++ Android/src/de/fltdpl/qmansis/Bluetooth.java
@@ -1,13 +1,18 @@
 package de.fltdpl.qmansis;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Bluetooth extends ActionBarActivity{
+	
+	BluetoothAdapter btAdapter;
+	
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -46,6 +51,26 @@ public class Bluetooth extends ActionBarActivity{
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_bluetooth);
 	        
+	        btAdapter = BluetoothAdapter.getDefaultAdapter();		// Init Bluetoothadapter
+	        if(btAdapter == null){									// Wenn kein Bluetooth vorhanden ist
+	        	Toast.makeText(getApplicationContext(), "Kein Bluetooth erkannt.", 0).show();
+	        	finish();
+	        } else {												// Bluetooth vorhanden
+	        	if(!btAdapter.isEnabled()){							// Bluetooth ausgeschaltet?
+	        		Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE); // Bluetooth anschalten
+	        		startActivityForResult(intent, 1);
+	        	}
+	        }
+	        
+	 }
+	 
+	 @Override
+	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		 super.onActivityResult(requestCode, resultCode, data);
+		 if(resultCode == RESULT_CANCELED){
+			 Toast.makeText(getApplicationContext(), "Bluetooth muss eingeschaltet sein.", Toast.LENGTH_SHORT).show();
+			 finish();
+		 }
 	 }
 	
 
